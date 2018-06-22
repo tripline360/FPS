3e76b9651936a22aaee3d96f3b24cdc8807856be
tripline360 <tripline360@gmail.com>
2018-06-22 20:42:00 +0930

    Second weapon
    Added second gun
    Toggled auto-fire

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -1,16 +1,90 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿
+//using UnityEngine;
 
-public class Shoot : MonoBehaviour {
+//public class Gun : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+//    public float damage = 10f;
+//    public float range = 100f;
+//    public float fireRate = 15f;
+//    public Camera fpsCam;
+//    public ParticleSystem muzzleFlash;
+//    public GameObject impactEffect;
+//    public GameObject impactEffectStone;
+//    public int ammo = 10;
+
+//    private float nextTimeToFire = 0f;
+
+
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+//        {
+//            nextTimeToFire = Time.time + 1f / fireRate;
+//            Shoot();
+//        }
+//    }
+
+//    void Shoot()
+//    {
+//        if (ammo <= 0)
+//        {
+//            return;
+//        }
+
+//        ammo = ammo - 1;
+
+
+//        muzzleFlash.Play();
+
+
+//        RaycastHit hit;
+//        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+//        {
+//            Debug.Log(hit.transform.name);
+
+//            Target target = hit.transform.GetComponent<Target>();
+//            if (target != null)
+//            {
+//                target.TakeDamage(damage);
+
+//            }
+
+//            bool playDefaultImpactEffect = false;
+
+//            ObjectProperties properties = hit.transform.GetComponent<ObjectProperties>();
+//            if (properties != null)
+//            {
+//                //if (properties.materialType == MaterialType.Stone)
+//                //{
+//                //    GameObject impactGO = Instantiate(impactEffectStone, hit.point, Quaternion.LookRotation(hit.normal));
+//                //    Destroy(impactGO, 2f);
+//                //}
+
+//                if (properties.impactEffect != null)
+//                {
+//                    GameObject impactGO = Instantiate(properties.impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+//                    Destroy(impactGO, 2f);
+//                }
+//                else
+//                {
+//                    playDefaultImpactEffect = true;
+//                }
+//            }
+//            else
+//            {
+//                playDefaultImpactEffect = true;
+//            }
+
+//            if (playDefaultImpactEffect == true)
+//            {
+//                // Play a default impact.
+//            }
+
+//            //GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+//            //Destroy(impactGO, 2f);
+//        }
+//    }
+//}
+
--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -4,31 +4,53 @@ using UnityEngine;
 public class Gun : MonoBehaviour {
 
     public float damage = 10f;
-
     public float range = 100f;
-
+    public float fireRate = 15f;
     public Camera fpsCam;
-
     public ParticleSystem muzzleFlash;
-
     public GameObject impactEffect;
-
     public GameObject impactEffectStone;
+    public int ammo = 10;
+
+    public bool autoFire = false;
+
+    private float nextTimeToFire = 0f;
 
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (autoFire == true)
         {
-            Shoot();
+            if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                Shoot();
+            }
         }
+        else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        
     }
 
     void Shoot()
     {
+        if (ammo <= 0)
+        {
+            return;
+        }
+
+        ammo = ammo - 1;
+       
+
         muzzleFlash.Play();
+
 
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
