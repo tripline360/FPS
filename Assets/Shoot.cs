9eca2f00c6604ec1b463a2bc18e0b2b083b1bffe
tripline360 <tripline360@gmail.com>
2018-06-22 22:10:48 +0930

    Reload
    Guns can now Reload

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -1,27 +1,40 @@
 ﻿
 using UnityEngine;
-
+using System.Collections;
 public class Gun : MonoBehaviour {
 
     public float damage = 10f;
     public float range = 100f;
     public float fireRate = 15f;
+    private bool isReloading = false;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
     public GameObject impactEffectStone;
-    public int ammo = 10;
+
+    public int maxAmmo = 10;
+    public float reloadTime = 1f;
+    private int currentAmmo;
 
     public bool autoFire = false;
 
     private float nextTimeToFire = 0f;
+
+    void Start ()
+    {
+        currentAmmo = maxAmmo;
+    }
 
 
 
     // Update is called once per frame
     void Update()
     {
-        if (autoFire == true)
+        if (isReloading)
+            return;
+
+        if (autoFire)
         {
             if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
             {
@@ -41,12 +54,13 @@ public class Gun : MonoBehaviour {
 
     void Shoot()
     {
-        if (ammo <= 0)
+        if (currentAmmo <= 0)
         {
+            StartCoroutine(Reload());
             return;
         }
 
-        ammo = ammo - 1;
+        currentAmmo = currentAmmo - 1;
        
 
         muzzleFlash.Play();
@@ -97,6 +111,23 @@ public class Gun : MonoBehaviour {
 
             //GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             //Destroy(impactGO, 2f);
+            
+
         }
+
+        
+    }
+
+    IEnumerator Reload()
+    {
+        Debug.Log("Reloading...");
+
+        isReloading = true;
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
+
+        isReloading = false;
     }
 }
